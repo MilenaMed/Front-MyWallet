9481424130d7760cad433d9830d3e15cad0a65e8
MilenaMed <milenamfm@gmail.com>
2023-07-11 15:43:13 -0300

    Cadastro

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,6 +1,9 @@
 import styled from "styled-components"
 import { Link } from "react-router-dom"
 import MyWalletLogo from "../components/MyWalletLogo"
+import { useNavigate } from "react-router-dom";
+import axios from "axios"
+import { useContext } from "react";
 
 export default function SignInPage() {
   return (
@@ -8,11 +11,11 @@ export default function SignInPage() {
       <form>
         <MyWalletLogo />
         <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
+        <input placeholder="Senha" type="password" autoComplete="new-password" />
         <button>Entrar</button>
       </form>
 
-      <Link>
+      <Link to={"/cadastro"}>
         Primeira vez? Cadastre-se!
       </Link>
     </SingInContainer>
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,20 +1,67 @@
 import { Link } from "react-router-dom"
 import styled from "styled-components"
 import MyWalletLogo from "../components/MyWalletLogo"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
+import axios from "axios"
+
 
 export default function SignUpPage() {
+  const navigate = useNavigate()
+  const [name, setNomeUsuario] = useState("")
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [ConfirmaçãoSenha, setConfirmaçãoSenha] = useState("");
+
+  function passarDados(event) {
+    event.preventDefault()
+    const EmailValido = /\S+@\S+\.\S+/;
+    const dados = { name, email, senha}
+
+    if (name.length > 0 && (email.search(EmailValido) !== -1) && email.length > 11 && senha.length >= 3 && senha === ConfirmaçãoSenha) {
+      const promise = axios.post(`${process.env.REACT_APP_API_URL}/cadastro`, dados)
+        promise.then(response => {
+            alert("Usuário cadastrado com sucesso")
+            navigate('/')
+        })
+        promise.catch((err) => {
+            console.log(err)
+        })
+    } else {
+      alert("erro: preencha os dados corretamente")
+    }
+
+  }
   return (
     <SingUpContainer>
       <form>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
-        <button>Cadastrar</button>
+        <input
+          placeholder="Nome"
+          type="text"
+          required value={name}
+          onChange={(e) => setNomeUsuario(e.target.value)} />
+        <input
+          placeholder="E-mail"
+          type="email"
+          required value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          placeholder="Senha"
+          type="password"
+          autoComplete="new-password"
+          required value={senha}
+          onChange={(e) => setSenha(e.target.value)} />
+        <input placeholder="Confirme a senha"
+          type="password"
+          autoComplete="new-password"
+          required value={ConfirmaçãoSenha}
+          onChange={(e) => setConfirmaçãoSenha(e.target.value)} />
+        <button onClick={passarDados} >Cadastrar</button>
       </form>
 
-      <Link>
+      <Link to={"/"}>
         Já tem uma conta? Entre agora!
       </Link>
     </SingUpContainer>
